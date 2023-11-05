c67ede375fea9ab9b19fcf68c365258a5651b455
nogElBoss <gn16102000@gmail.com>
2023-11-05 15:41:44 +0000

    first commit

---
+++ themes/theme.tsx
@@ -0,0 +1,17 @@
+import { extendTheme } from "@chakra-ui/react"
+
+const breakpoints = {
+    sm: '40em',
+    md: '52em',
+    lg: '64em',
+    xl: '80em',
+}
+
+const theme = extendTheme({
+    colors: {
+        red: '#EC1D23',
+        bgColor: "DEDEDE",
+    },
+})
+
+export default theme
